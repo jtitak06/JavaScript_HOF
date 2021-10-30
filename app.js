15a221db87544d3f536b3492a9cb653f2d58544f
Jeff Titak <jtitak06@yahoo.com>
2021-10-30 19:40:57 +0300

    exercise solved

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
+function plus(number) {
+    return function anonymous(plusNumber) {
+        return plusNumber + number
+    }
+}
+
+let plus15 = plus(15);
+console.log(plus15(10));
 console.log("EXERCISE 1:\n==========\n");
